bc0c8a86f94f9bb1204e1ef14dfe846382a23da2
彭伟中 <pengwz@trumgu.com>
2024-08-31 21:55:55 +0800

    init.

---
+++ src/main/java/com/pengwz/dynamic2/sql/anno/Table.java
@@ -0,0 +1,27 @@
+package com.pengwz.dynamic2.sql.anno;
+
+import java.lang.annotation.*;
+
+/**
+ * 表注解，作用在实体类类名上，value 和数据库表对应
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Table {
+    /**
+     * 表名
+     *
+     * @return 返回trim()后的表名
+     */
+    String value() default "";
+
+    /**
+     * 是否将该表实体缓存到程序缓存中。默认缓存。
+     *
+     * @return 是否缓存表实体
+     */
+    boolean isCache() default true;
+
+
+}
