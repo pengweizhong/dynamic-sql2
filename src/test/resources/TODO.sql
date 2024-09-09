7034637becdb0491a2474081dc712ecf33159517
pengweizhong <pengwz@hotmail.com>
2024-09-09 10:06:19 +0800

    todo

---
+++ src/test/resources/TODO.sql
@@ -0,0 +1,22 @@
+-- 把这种SQL实现一下：
+SELECT
+    CASE
+        WHEN current_month_ret > 10 THEN '>10%'
+        WHEN current_month_ret BETWEEN 5 AND 10 THEN '5~10%'
+        WHEN current_month_ret BETWEEN 0 AND 5 THEN '0~5%'
+        WHEN current_month_ret = 0 THEN '0%'
+        WHEN current_month_ret BETWEEN -5 AND 0 THEN '0~-5%'
+        WHEN current_month_ret BETWEEN -10 AND -5 THEN '-5~-10%'
+        WHEN current_month_ret < -10 THEN '<-10%'
+        END AS profit_range,
+    COUNT(*) AS product_count,
+    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM t_pro_ret_phased), 2) AS percentage
+FROM t_pro_ret_phased where current_month_ret is not null
+GROUP BY profit_range
+ORDER BY FIELD(profit_range, '>10%', '5~10%', '0~5%', '0%', '0~-5%', '-5~-10%', '<-10%');
+
+-- 查出的结果：
+profit_range|product_count|percentage|
+------------+-------------+----------+
+0~5%        |         4150|     43.56|
+0~-5%       |         5372|     56.39|
