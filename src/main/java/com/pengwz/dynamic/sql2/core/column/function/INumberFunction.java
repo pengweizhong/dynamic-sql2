90377afaeeecbe3767c262b4c14bcd596bfdf15d
pengweizhong <pengwz@hotmail.com>
2024-09-14 11:40:01 +0800

    add 日期、数字、字符串的顶级函数接口

---
+++ src/main/java/com/pengwz/dynamic/sql2/core/column/function/INumberFunction.java
@@ -0,0 +1,17 @@
+package com.pengwz.dynamic.sql2.core.column.function;
+
+/**
+ * •	ABS(x): 返回 x 的绝对值。
+ * •	CEIL(x) 或 CEILING(x): 向上取整到最接近的整数。
+ * •	FLOOR(x): 向下取整到最接近的整数。
+ * •	ROUND(x, d): 将 x 四舍五入到 d 位小数。
+ * •	POW(x, y) 或 POWER(x, y): 返回 x 的 y 次幂。
+ * •	SQRT(x): 返回 x 的平方根。
+ * •	EXP(x): 返回 e 的 x 次幂。
+ * •	LOG(x): 返回 x 的自然对数。
+ * •	LOG10(x): 返回 x 的以 10 为底的对数。
+ */
+public interface INumberFunction extends IColumFunction {
+
+
+}
---
+++ src/main/java/com/pengwz/dynamic/sql2/core/column/function/IStringFunction.java
@@ -0,0 +1,13 @@
+package com.pengwz.dynamic.sql2.core.column.function;
+
+/**
+ * •	CONCAT(str1, str2, ...): 连接两个或多个字符串。
+ * •	SUBSTRING(str, start, length): 从字符串 str 中提取子串。
+ * •	LENGTH(str) 或 LEN(str): 返回字符串的长度。
+ * •	TRIM(str): 删除字符串前后的空格。
+ * •	REPLACE(str, search, replace): 替换字符串中的子串。
+ */
+public interface IStringFunction extends IColumFunction {
+
+
+}
