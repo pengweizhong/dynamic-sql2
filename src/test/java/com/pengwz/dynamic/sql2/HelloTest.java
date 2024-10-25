799dcddec9c892e5b8922aa6f085747ed428caa7
pengweizhong <pengwz@hotmail.com>
2024-10-25 11:34:56 +0800

    fix

--- src/test/java/com/pengwz/dynamic/sql2/HelloTest.java
+++ src/test/java/com/pengwz/dynamic/sql2/HelloTest.java
@@ -1,15 +1,11 @@
 package com.pengwz.dynamic.sql2;
 
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.page.PageMethod;
 import org.junit.jupiter.api.Test;
 
 class HelloTest {
 
     @Test
     void test() {
-        PageHelper.startPage(1, 2).doSelectPageInfo(() -> System.getenv());
-        PageMethod.startPage(1,1).doSelectPage(() -> System.getenv()).getPages();
         System.out.println("Hello World");
     }
 }
