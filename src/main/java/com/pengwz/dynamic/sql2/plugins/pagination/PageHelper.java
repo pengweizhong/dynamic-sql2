859ddcf9b765d7d5689f7bb884bca4e2087794df
pengweizhong <pengwz@hotmail.com>
2024-10-25 10:01:59 +0800

    文档注释

--- src/main/java/com/pengwz/dynamic/sql2/plugins/pagination/PageHelper.java
+++ src/main/java/com/pengwz/dynamic/sql2/plugins/pagination/PageHelper.java
@@ -10,34 +10,69 @@ import java.util.function.Supplier;
 import static com.pengwz.dynamic.sql2.plugins.pagination.LocalPage.clearCurrentPage;
 import static com.pengwz.dynamic.sql2.plugins.pagination.LocalPage.setCurrentPage;
 
+/**
+ * PageHelper 用于处理不同类型的数据分页逻辑的辅助类。
+ * <p>
+ * {@link this#of(int, int)}：通用分页逻辑<br>
+ * {@link this#ofCollection(int, int)}：集合类型分页逻辑<br>
+ * {@link this#ofMap(int, int)}：Map类型分页逻辑
+ */
 public class PageHelper {
     private static final Logger log = LoggerFactory.getLogger(PageHelper.class);
 
     private PageHelper() {
     }
 
+    /**
+     * 创建一个通用的 GeneralPageHelper 实例，指定页码和页大小。
+     *
+     * @param pageIndex 要获取的页码（如果小于等于0，则页码默认为1）
+     * @param pageSize  每页的大小（每页的项目数量）
+     * @return GeneralPageHelper 实例，用于通用的分页处理
+     * @throws IllegalArgumentException 如果 pageSize 小于等于0
+     */
     public static GeneralPageHelper of(int pageIndex, int pageSize) {
         checkPageParams(pageIndex, pageSize);
         return new GeneralPageHelper(Math.max(pageIndex, 1), pageSize);
+    }
+
+    /**
+     * 创建一个 CollectionPageHelper 实例，指定页码和页大小。
+     * 针对处理集合类型的分页。
+     *
+     * @param pageIndex 要获取的页码（如果小于等于0，则页码默认为1）
+     * @param pageSize  每页的大小（每页的项目数量）
+     * @return CollectionPageHelper 实例，用于集合的分页处理
+     * @throws IllegalArgumentException 如果 pageSize 小于等于0
+     */
+    public static CollectionPageHelper ofCollection(int pageIndex, int pageSize) {
+        checkPageParams(pageIndex, pageSize);
+        return new CollectionPageHelper(Math.max(pageIndex, 1), pageSize);
+    }
+
+    /**
+     * 创建一个 MapPageHelper 实例，指定页码和页大小。
+     * 针对处理 Map 类型的分页。
+     *
+     * @param pageIndex 要获取的页码（如果小于等于0，则页码默认为1）
+     * @param pageSize  每页的大小（每页的项目数量）
+     * @return MapPageHelper 实例，用于 Map 的分页处理
+     * @throws IllegalArgumentException 如果 pageSize 小于等于0
+     */
+    public static MapPageHelper ofMap(int pageIndex, int pageSize) {
+        checkPageParams(pageIndex, pageSize);
+        return new MapPageHelper(Math.max(pageIndex, 1), pageSize);
     }
 
     static <T> GeneralPageHelper of(PageInfo<T> pageInfo) {
         return new GeneralPageHelper(pageInfo);
     }
 
-    public static CollectionPageHelper ofCollection(int pageIndex, int pageSize) {
-        checkPageParams(pageIndex, pageSize);
-        return new CollectionPageHelper(Math.max(pageIndex, 1), pageSize);
-    }
 
     static <C extends Collection<T>, T> CollectionPageHelper ofCollection(CollectionPage<C, T> collectionPage) {
         return new CollectionPageHelper(collectionPage);
     }
 
-    public static MapPageHelper ofMap(int pageIndex, int pageSize) {
-        checkPageParams(pageIndex, pageSize);
-        return new MapPageHelper(Math.max(pageIndex, 1), pageSize);
-    }
 
     static <K, V, M extends Map<K, V>> MapPageHelper ofMap(MapPage<K, V, M> mapPage) {
         return new MapPageHelper(mapPage);
