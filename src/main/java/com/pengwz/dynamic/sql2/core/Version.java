148d310b4fae8db2c6d5720124adfa615d73e9b9
pengweizhong <pengwz@hotmail.com>
2024-11-20 14:24:11 +0800

    add 增加版本号比较方法

--- src/main/java/com/pengwz/dynamic/sql2/core/Version.java
+++ src/main/java/com/pengwz/dynamic/sql2/core/Version.java
@@ -1,5 +1,7 @@
 package com.pengwz.dynamic.sql2.core;
 
+import java.util.Objects;
+
 public class Version {
     private int majorVersion;
     private int minorVersion;
@@ -25,5 +27,35 @@ public class Version {
         return patchVersion;
     }
 
+    public int compareTo(Version other) {
+        if (this.majorVersion != other.majorVersion) {
+            return this.majorVersion - other.majorVersion;
+        }
+        if (this.minorVersion != other.minorVersion) {
+            return this.minorVersion - other.minorVersion;
+        }
+        return this.patchVersion - other.patchVersion;
+    }
+
+    public boolean isGreaterThan(Version other) {
+        return this.compareTo(other) > 0;
+    }
+
+    public boolean isGreaterThanOrEqual(Version other) {
+        return this.compareTo(other) >= 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Version version = (Version) o;
+        return majorVersion == version.majorVersion && minorVersion == version.minorVersion && patchVersion == version.patchVersion;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(majorVersion, minorVersion, patchVersion);
+    }
 
 }
