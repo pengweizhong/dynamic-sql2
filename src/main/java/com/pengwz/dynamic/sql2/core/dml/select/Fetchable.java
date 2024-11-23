3257b91a98ab1b5e88b5f15fd53990c67527bd64
pengweizhong <pengwz@hotmail.com>
2024-11-24 00:32:32 +0800

    add fetchOriginalMap

--- src/main/java/com/pengwz/dynamic/sql2/core/dml/select/Fetchable.java
+++ src/main/java/com/pengwz/dynamic/sql2/core/dml/select/Fetchable.java
@@ -31,14 +31,13 @@ public interface Fetchable {
     /**
      * 获取查询结果并返回原始的 {@code  Map<String, Object>} 类型数据。
      * <p>
-     * 该方法将返回一个包含键值对的 {@link Map}，其中键是列名（或字段名），值是对应列的数据。
+     * 该方法强制转换查询结果为 {@code Map<String, Object>} 类型。
+     * 其中键是列名（或字段名），值是对应列的数据。
      * 这对于没有明确映射到实体类的查询非常有用，例如动态查询或需要临时处理的场景。
      * <p>
-     * 该方法强制转换 {@link FetchResult} 为 {@code Map<String, Object>} 类型。
      *
      * @return 查询结果的封装 {@code FetchResult<Map<String, Object>>}，包含了查询结果的数据。
      * 结果中的每一项都是一个 {@link Map}，其中键是字段名，值是字段的值。
-     * @throws ClassCastException 如果查询结果的类型不匹配，可能会抛出类型转换异常。
      */
     @SuppressWarnings({"unchecked", "rawtypes"})
     default FetchResult<Map<String, Object>> fetchOriginalMap() {
