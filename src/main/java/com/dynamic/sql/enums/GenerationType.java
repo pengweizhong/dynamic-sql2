1cc85da7a81e95aefd5f5a9f353adc99f81914a3
pengweizhong <pengwz@hotmail.com>
2025-01-17 22:23:44 +0800

    删除GenerationType注释

--- src/main/java/com/dynamic/sql/enums/GenerationType.java
+++ src/main/java/com/dynamic/sql/enums/GenerationType.java
@@ -11,27 +11,10 @@ public enum GenerationType {
      */
     AUTO,
 
-//    /**
-//     * 使用UUID进行自增
-//     */
-//    UUID,
-//    /**
-//     * 使用大写的UUID进行自增
-//     */
-//    UPPER_UUID,
-//    /**
-//     * 使用简洁的UUID进行自增，该UUID是不包含 ‘-’的
-//     */
-//    SIMPLE_UUID,
-//    /**
-//     * 使用大写的UUID进行自增，该UUID是不包含 ‘-’的
-//     */
-//    UPPER_SIMPLE_UUID,
     /**
      * 使用序列进行自增，当使用序列类型自增时，需要执行序列名。目前已验证的仅Oracle支持
      *
      * @see GeneratedValue#sequenceName()
      */
     SEQUENCE,
-
 }
