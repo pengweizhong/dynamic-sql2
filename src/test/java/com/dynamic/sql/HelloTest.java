42f442e41753a05e523e12f16ad31c3bf1c4ed9a
pengwz <pengwz@hotmail.com>
2025-01-23 14:47:57 +0800

    test

--- src/test/java/com/dynamic/sql/HelloTest.java
+++ src/test/java/com/dynamic/sql/HelloTest.java
@@ -2,11 +2,15 @@ package com.dynamic.sql;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.YearMonth;
+
 public class HelloTest {
 
     @Test
     void test() {
         System.out.println("Hello World");
+        String string = YearMonth.of(2012, 12).toString();
+        System.out.println(string);
     }
 }
 
