3f160acbc6f16aeebff7b9de673ec015041cc621
pengwz <pengwz@hotmail.com>
2025-07-22 13:38:58 +0800

    test

---
+++ src/test/java/com/dynamic/sql/core/dml/select/SelectAPTTest.java
@@ -0,0 +1,15 @@
+package com.dynamic.sql.core.dml.select;
+
+import com.dynamic.sql.InitializingContext;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SelectAPTTest extends InitializingContext {
+    private static final Logger log = LoggerFactory.getLogger(SelectTest.class);
+
+    @Test
+    void test() {
+        log.info("test");
+    }
+}
