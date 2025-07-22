b8c101cfbd189aa7f4e8de00b39cac8716d0b78d
pengwz <pengwz@hotmail.com>
2025-07-22 17:37:03 +0800

    更新合适的列包装名类名

--- src/test/java/com/dynamic/sql/core/dml/select/SelectAPTTest.java
+++ src/test/java/com/dynamic/sql/core/dml/select/SelectAPTTest.java
@@ -1,9 +1,12 @@
 package com.dynamic.sql.core.dml.select;
 
 import com.dynamic.sql.InitializingContext;
+import com.dynamic.sql.entites.User;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class SelectAPTTest extends InitializingContext {
     private static final Logger log = LoggerFactory.getLogger(SelectTest.class);
@@ -11,5 +14,13 @@ public class SelectAPTTest extends InitializingContext {
     @Test
     void test() {
         log.info("test");
+        List<User> list = sqlContext.select()
+                .column(User::getUserId)
+                .column(User::getName)
+                .from(User.class)
+                .fetch()
+                .toList();
+        System.out.println(list);
+        System.out.println(list.size());
     }
 }
