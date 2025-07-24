ca248cf01b0b53703b7bd9d1cb179db842ea962a
pengwz <pengwz@hotmail.com>
2025-07-24 20:28:26 +0800

    CaseWhen

--- src/main/java/com/dynamic/sql/core/column/function/logical/CaseWhen.java
+++ src/main/java/com/dynamic/sql/core/column/function/logical/CaseWhen.java
@@ -1,62 +1,48 @@
 package com.dynamic.sql.core.column.function.logical;//package com.pengwz.dynamic.sql2.core.column.function.logical;
-//
-//import com.pengwz.dynamic.sql2.core.Fn;
-//import com.pengwz.dynamic.sql2.core.column.function.ColumFunction;
-//import com.pengwz.dynamic.sql2.core.column.function.ColumnFunctionDecorator;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class CaseWhen extends ColumnFunctionDecorator {
-//
-//    private final List<String> conditions = new ArrayList<>();
-//    private String elseCondition;
-//
-//    // 构造方法
-//    protected CaseWhen(ColumFunction delegateFunction) {
-//        super(delegateFunction);
-//    }
-//
-//    protected <T, F> CaseWhen(Fn<T, F> fn) {
-//        super(fn);
-//    }
-//
-//    public static <T, F> CaseWhenBuilder builder(Fn<T, F> fn) {
-//        return new CaseWhenBuilder(new CaseWhen(fn));
-//    }
-//
-//    public static CaseWhenBuilder builder(ColumFunction delegateFunction) {
-//        return new CaseWhenBuilder(new CaseWhen(delegateFunction));
-//    }
-//
-//    @Override
-//    public String getMySqlFunction() {
-//        StringBuilder caseExpression = new StringBuilder("case ");
-//        for (String condition : conditions) {
-//            caseExpression.append(condition).append(" ");
-//        }
-//        if (elseCondition != null) {
-//            caseExpression.append(elseCondition).append(" ");
-//        }
-//        caseExpression.append("end");
-//        return caseExpression.toString();
-//    }
-//
-//    public static class CaseWhenBuilder {
-//        private CaseWhen caseWhen;
-//
-//        public CaseWhenBuilder(CaseWhen caseWhen) {
-//            this.caseWhen = caseWhen;
-//        }
-//
-//        public CaseWhen build() {
-//            return caseWhen;
-//        }
-//    }
-//
-//    // 嵌套条件选择器
-//    public static class ConditionalSelector {
-//
-//    }
-//
-//}
+
+
+import com.dynamic.sql.core.Version;
+import com.dynamic.sql.core.column.function.AbstractColumFunction;
+import com.dynamic.sql.core.column.function.ColumnFunctionDecorator;
+import com.dynamic.sql.core.condition.WhereCondition;
+import com.dynamic.sql.enums.SqlDialect;
+import com.dynamic.sql.model.TableAliasMapping;
+
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class CaseWhen extends ColumnFunctionDecorator {
+
+    public CaseWhen(AbstractColumFunction delegateFunction) {
+        super(delegateFunction);
+    }
+
+    public CaseWhen(Consumer<WhereCondition> condition) {
+
+    }
+
+    public CaseWhenBuilder else_(Object elseValue) {
+        return null;
+    }
+
+    public CaseWhenBuilder otherwise(Object elseValue) {
+        return else_(elseValue);
+    }
+
+
+    @Override
+    public String getFunctionToString(SqlDialect sqlDialect, Version version, Map<String, TableAliasMapping> aliasTableMap) throws UnsupportedOperationException {
+
+        return "";
+    }
+
+    public static class CaseWhenBuilder {
+        private CaseWhen caseWhen;
+
+
+        public CaseWhen build() {
+            return caseWhen;
+        }
+
+    }
+}
